9090ab587ae96f4a133a5c226186fe917d63e710
Risbern21 <risbernfernandes21@gmail.com>
2025-03-19 16:46:32 +0530

    changed env variables

--- components/contact/Form.jsx
+++ components/contact/Form.jsx
@@ -36,11 +36,11 @@ export default function Form() {
     const toastId = toast.loading("Sending Your Message,Please Wait...");
     emailjs
       .send(
-        `${process.env.SERVICE_ID}`,
-        `${process.env.TEMPLATE_ID}`,
+        `${process.env.NEXT_PUBLIC_SERVICE_ID}`,
+        `${process.env.NEXT_PUBLIC_TEMPLATE_ID}`,
         params,
         {
-          publicKey: `${process.env.PUBLIC_KEY}`,
+          publicKey: `${process.env.NEXT_PUBLIC_PUBLIC_KEY}`,
           limitRate: {
             throttle: 5000,
           },
